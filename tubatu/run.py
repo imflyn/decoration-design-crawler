93acd3e74ad46ce27cd074c2a02154d4a294f59f
flyn.yu <flyn.yu@fandine.com>
2016-09-30 15:04:45 +0800

    restart spider if spider closed

--- tubatu/run.py
+++ tubatu/run.py
@@ -2,6 +2,8 @@ import os
 import sys
 from os.path import dirname
 
+from twisted.internet import reactor
+
 path = dirname(os.path.abspath(os.path.dirname(__file__)))
 sys.path.append(path)
 
@@ -9,19 +11,31 @@ from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from scrapy import signals
 from pydispatch import dispatcher
-from msic.proxy.proxy_pool import proxy_pool
 from tubatu.spiders.room_spider import RoomSpider
 
-proxy_pool.start()
 
-process = CrawlerProcess(get_project_settings())
+class Runner(object):
+	def __init__(self):
+		dispatcher.connect(self.crawl, signals.engine_stopped)
+		self.setting = get_project_settings()
+		self.process = None
+
+	def start_scrapy(self):
+		self.process = CrawlerProcess(self.setting)
+		self.crawl()
+		reactor.run()
+
+	def crawl(self):
+		self.process.crawl(RoomSpider())
+
+	def start_proxy_pool(self):
+		from msic.proxy.proxy_pool import proxy_pool
+		proxy_pool.start()
+
+	def run(self):
+		self.start_proxy_pool()
+		self.start_scrapy()
 
 
-def start_scrapy():
-	process.stop()
-	process.crawl(RoomSpider())
-	process.start()
-
-
-dispatcher.connect(start_scrapy, signals.engine_stopped)
-start_scrapy()
+if __name__ == '__main__':
+	Runner().run()
