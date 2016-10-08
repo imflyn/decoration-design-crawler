0cf1b117ea63fcb4af7da38371bdec4b7fbdce11
flyn.yu <flyn.yu@fandine.com>
2016-10-08 16:12:11 +0800

    set request delay time

--- tubatu/tubatu/settings.py
+++ tubatu/tubatu/settings.py
@@ -33,7 +33,7 @@ REACTOR_THREADPOOL_MAXSIZE = 8
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 0.6
+DOWNLOAD_DELAY = 1
 # The download delay setting will honor only one of:
 # CONCURRENT_REQUESTS_PER_DOMAIN = 16
 # CONCURRENT_REQUESTS_PER_IP = 16
