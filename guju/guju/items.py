f3a15bf7b51efe4bf14578cd7408212fb00df0d2
flyn.yu <flyn.yu@fandine.com>
2016-11-15 14:58:08 +0800

    谷居装修爬虫代码

---
+++ guju/guju/items.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class DesignStrategyItem(scrapy.Item):
+	title = scrapy.Field()
+	sub_title = scrapy.Field()
+	html_url = scrapy.Field()
+	description = scrapy.Field()
+	content = scrapy.Field()
+	category = scrapy.Field()
